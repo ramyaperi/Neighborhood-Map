e450d1373aa7b166fd44a6a3a2c21afe1a1ff5d5
ramyaperi <30437760+ramyaperi@users.noreply.github.com>
2017-11-12 15:09:44 +0100

    initial commit

---
+++ js/mapview.js
@@ -0,0 +1,49 @@
+$(document).ready(function () {
+    ko.applyBindings(viewModel);
+});
+
+//map model for init
+var mapModel  = {
+          lat: 48.135268,
+          lng: 11.583283
+      };
+
+//ViewModel for map
+var mapViewModel = function() {
+    var self = this;
+    self.mapView = ko.observable(mapModel);
+
+    self.menubuttonclicked = function(){
+
+    };
+}
+
+
+/*
+custom binding to bind the google map object
+ */
+    ko.bindingHandlers.map = {
+        init: function (element, valueAccessor, allBindingsAccessor, viewModel) {
+            var mapObj = ko.utils.unwrapObservable(valueAccessor());
+            var latLng = new google.maps.LatLng(
+                ko.utils.unwrapObservable(mapObj.lat),
+                ko.utils.unwrapObservable(mapObj.lng));
+            //map option for custom map loading
+            var mapOptions = {
+                center: latLng,
+                zoom: 13,
+                mapTypeId: google.maps.MapTypeId.ROADMAP,
+                mapTypeControl: false,
+                disableDefaultUI: true
+            };
+
+            mapObj.googleMap = new google.maps.Map(element, mapOptions);
+
+        }
+    };
+
+
+
+var viewModel = new mapViewModel();
+
+
