f25814896e56d91d198583d1b7134ea5de7733f9
ramyaperi <30437760+ramyaperi@users.noreply.github.com>
2017-11-13 21:47:11 +0100

    markers and side menu

--- js/mapview.js
+++ js/mapview.js
@@ -3,21 +3,76 @@ $(document).ready(function () {
 });
 
 //map model for init
-var mapModel  = {
-          lat: 48.135268,
-          lng: 11.583283
-      };
+var model = {
+//map model for init
+    maplocation: {
+        lat: 48.135268,
+        lng: 11.583283
+    },
 
+    mapMarkers: [
+        {
+            title: "Englischer Garten",
+            position:
+                {
+                    lat: 48.164308,
+                    lng: 11.605395
+                }
+        },
+        {
+            title: "Surfers corner",
+            position:
+                {
+                    lat: 48.143733,
+                    lng: 11.588045
+                }
+        },
+        {
+            title: "Marienplatz/Town Hall",
+            position:
+                {
+                    lat: 48.1383715,
+                    lng: 11.5708304
+                }
+        },
+        {
+            title: "St. Peter's Church / Peterskirche",
+            position:
+                {
+                    lat: 48.1367954,
+                    lng: 11.5720024
+                }
+        },
+        {
+            title: "Hofbräuhaus",
+            position:
+                {
+                    lat: 48.1371928,
+                    lng: 11.5764119
+                }
+        },
+
+    ],
+    currentmarkers: []
+};
 //ViewModel for map
 var mapViewModel = function() {
     var self = this;
-    self.mapView = ko.observable(mapModel);
+    self.mapView = ko.observable(model);
 
-    self.menubuttonclicked = function(){
-        $("#menu-toggle").click(function (e) {
-            e.preventDefault();
-            $("#wrapper").toggleClass("toggled");
-        });
+    self.mapselected = function () {
+        // hide the sidebar
+        $('#sidebar').removeClass('active');
+        // fade out the overlay
+        $('.overlay').fadeOut();
+    };
+
+    self.menuselected = function () {
+        $('#sidebar').addClass('active');
+        // fade in the overlay
+        $('.overlay').fadeIn();
+        $('.collapse.in').toggleClass('in');
+        $('a[aria-expanded=true]').attr('aria-expanded', 'false');
     };
 }
 
@@ -27,7 +82,8 @@ custom binding to bind the google map object
  */
     ko.bindingHandlers.map = {
         init: function (element, valueAccessor, allBindingsAccessor, viewModel) {
-            var mapObj = ko.utils.unwrapObservable(valueAccessor());
+            var model = ko.utils.unwrapObservable(valueAccessor());
+            var mapObj = model.maplocation;
             var latLng = new google.maps.LatLng(
                 ko.utils.unwrapObservable(mapObj.lat),
                 ko.utils.unwrapObservable(mapObj.lng));
@@ -41,6 +97,15 @@ custom binding to bind the google map object
             };
 
             mapObj.googleMap = new google.maps.Map(element, mapOptions);
+            model.mapMarkers.forEach(function (marker) {
+                mapObj.marker = new google.maps.Marker({
+
+                    position: marker.position,
+                    title: marker.title,
+                    map: mapObj.googleMap
+                });
+                // model.currentmarkers.push(mapObj.marker);
+            });
 
         }
     };
